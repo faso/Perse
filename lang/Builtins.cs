97a00c4f08d35f9d50d52a4af22289129bc109dd
faso <yigbot@gmail.com>
2017-05-05 16:00:41 +0300

    len, first, last

--- lang/Builtins.cs
+++ lang/Builtins.cs
@@ -4,29 +4,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BuiltIntFunction = System.Func<Lang.Objects.ILangObject[], Lang.Objects.ILangObject>;
 
 namespace Lang.Builtins
 {
     public static class BuiltinFunctions
     {
-        public static Func<ILangObject[], ILangObject> Len = o => 
+        public static BuiltIntFunction Len = o => 
         {
             if (o.Length != 1)
                 return new LangError($"Wrong number of arguments. Expected 1, got {o.Length}");
 
-            if (!(o[0] is LangString))
+            if (!(o[0] is LangString) && !(o[0] is LangArray))
                 return new LangError($"Wrong argument type for 'len'");
 
-            return new LangInteger() { Value = (o[0] as LangString).Value.Length };
+            if (o[0] is LangString)
+                return new LangInteger() { Value = (o[0] as LangString).Value.Length };
+            else
+            {
+                return new LangInteger() { Value = (o[0] as LangArray).Elements.Count };
+            }
         };
 
-        public static Func<ILangObject[], ILangObject> Concat = o => new LangString() { Value = (o[0] as LangString).Value + (o[1] as LangString).Value };
+        public static BuiltIntFunction First = o =>
+        {
+            if (o.Length != 1)
+                return new LangError($"Wrong number of arguments. Expected 1, got {o.Length}");
+
+            if (!(o[0] is LangArray))
+                return new LangError($"Wrong argument type for 'first'");
+
+            return (o[0] as LangArray).Elements.First();
+        };
+
+        public static BuiltIntFunction Last = o =>
+        {
+            if (o.Length != 1)
+                return new LangError($"Wrong number of arguments. Expected 1, got {o.Length}");
+
+            if (!(o[0] is LangArray))
+                return new LangError($"Wrong argument type for 'first'");
+
+            return (o[0] as LangArray).Elements.Last();
+        };
+
+        public static BuiltIntFunction Concat = o => new LangString() { Value = (o[0] as LangString).Value + (o[1] as LangString).Value };
 
 
         public static Dictionary<string, ILangObject> Builtins = new Dictionary<string, ILangObject>()
         {
-            { "len", new Builtin() { Fn = Len } },
-            { "concat", new Builtin() { Fn = Concat } }
+            { "length", new Builtin() { Fn = Len } },
+            { "concat", new Builtin() { Fn = Concat } },
+            { "first", new Builtin() { Fn = First } },
+            { "last", new Builtin() { Fn = Last } }
         };
     }
 }
